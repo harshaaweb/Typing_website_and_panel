16c2a4f157d39d4582a9b04be146c1f319716e6c
info@harshaweb.com <info@harshaweb.com>
2023-02-21 17:33:27 +0530

    new

--- Webs/dbconfig.php
+++ Webs/dbconfig.php
@@ -1,9 +1,8 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "", "harsha_typing");
+    $conn = mysqli_connect("wcp.dauqu.com:3306", "harsha_typing", "harsha_typing", "harsha_typing");
 //   if($conn){
 //     echo "connected";
 //   }
 //     else{
 //         echo "not connected";
 //     }
-?>
