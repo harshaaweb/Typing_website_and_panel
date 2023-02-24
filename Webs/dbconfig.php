39a67798ea56ca2fec4eca52714175f389ddb928
info@harshaweb.com <info@harshaweb.com>
2023-02-24 12:21:40 +0530

    new 24 date

--- Webs/dbconfig.php
+++ Webs/dbconfig.php
@@ -2,6 +2,7 @@
 
     $conn = mysqli_connect("localhost", "jyotiinstitute_php", "Harsh@Singh8576", "jyotiinstitute_php");
     // $conn = mysqli_connect("localhost", "root", "", "harsha_typing");
+    // $conn = mysqli_connect("localhost", "root", "", "harsha_typing");
 
 //   if($conn){
 //     echo "connected";
